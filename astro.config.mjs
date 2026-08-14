e3d36d6ea7516f6c6dcecc1477f3e2db8af7221f
mrusskik <mrusskik@nd.edu>
2026-08-14 16:01:34 -0400

    Revert "Merge pull request #1 from sericson0/dance-toolkit"
    This reverts commit 32e3735cbc360ecee51bb94a4b3c386e4a295d17, reversing
    changes made to a9d89d525a91fd680fb1ae9ca02cd632f1b21b15.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   output: 'static',
   site: 'https://tangotoolkit.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [sitemap()],
 });
